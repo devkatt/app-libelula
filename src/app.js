54c87bdf48fe0d1c5cc7f2ab57fb5e5a4065ece9
Katty-Alves <89870229+Katty-Alves@users.noreply.github.com>
2022-07-08 17:45:55 +0000

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -3,7 +3,9 @@ const env = require('dotenv/config');
 
 const database = require('./db');
 const app=express();
+const cors = require('cors');
 app.use(express.json());
+app.use(cors());
 
 const libelulaRouter = require('./routers/libelula/libelulaRouter');
 const carrinhoRouter=require('./routers/libelula/carrinhoRouter');
@@ -16,7 +18,7 @@ app.use('/cliente', clienteRouter);
 app.use('/produto', produtoRouter);
 
 app.listen(process.env.PORT || 3000, async ()=>{
-    const resultDb = await database.sync({force:true});
+    const resultDb = await database.sync({alter:true});
     console.log('server started');
 })
 
